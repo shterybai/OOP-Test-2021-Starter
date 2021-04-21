a04ec9b3591a145d07f40a6de2c0207a0de00cd2
shterybai <andrewkennan2000@gmail.com>
2021-04-21 17:31:30 +0100

    Relevant checks for String score put in place, having trouble with the ArrayList

--- java/src/ie/tudublin/Note.java
+++ java/src/ie/tudublin/Note.java
@@ -7,19 +7,11 @@ public class Note {
     // Private fields
     private char note;
     private int duration;
-    private String name;
-
-
-    // Constructor Chaining method
-    public Note(TableRow row) {
-        this(row.getChar("note"), row.getInt("duration"), row.getString("name"));
-    }
 
     // Constructor method
-    public Note(char note, int duration, String name) {
+    public Note(char note, int duration) {
         this.note = note;
         this.duration = duration;
-        this.name = name;
     }
 
     // Getters and Setters
@@ -39,17 +31,9 @@ public class Note {
         this.duration = duration;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     // toString() method
     @Override
     public String toString() {
-        return "Note [note=" + note + ", duration=" + duration + ", name=" + name + "]";
+        return "Note [note=" + note + ", duration=" + duration + "]";
     }
 }
--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -9,23 +9,46 @@ import processing.data.TableRow;
 
 public class ScoreDisplay extends PApplet
 {
-	String score = "DEFGABcd";
+	// String score = "DEFGABcd";
 	//String score = "D2E2F2G2A2B2c2d2";
-	//String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
+	String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
 
 
 	// Array List
 	ArrayList<Note> note = new ArrayList<Note>();
 
+	// Loading the score
 	void loadScore() {
-		Table table = loadTable("notes.csv", "header");
+		for (int i = 0; i < score.length(); i++) {
+			// String s = score.substring(i);
+			// String ss = score.substring(score.charAt(i + 1));
 
-		for(TableRow row : table.rows()) {
-			Note n = new Note(row);
-			note.add(n);
+			char c = score.charAt(i);
+			char cc = score.charAt(i + 1);
+
+			boolean checki, checki1;
+			checki = Character.isDigit(c);
+			checki1 = Character.isDigit(cc);
+			if (checki == false) {
+				if (checki1 == false) {
+					System.out.println(c + "1" + "Quaver");
+					// note.add(new Note(c, 1));
+				}
+				else {
+					System.out.println(c + "2" + "Crotchet");
+					// note.add(new Note(c, 2));
+				}
+			}
 		}
 	}
 
+	// Printing the scores
+	// void printScore() {
+	// 	for (int i = 0; i < score.length(); i++) {
+	// 		Note n = note.get(i);
+	// 		System.out.println(n);
+	// 	}
+	// }
 	
 	public void settings()
 	{
@@ -39,7 +62,7 @@ public class ScoreDisplay extends PApplet
 
 	public void setup() 
 	{
-		
+		loadScore();
 	}
 
 	public void draw()
