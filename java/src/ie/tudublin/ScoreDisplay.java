c938222ea563aae9e49451656a339e3ae7cd6911
shterybai <andrewkennan2000@gmail.com>
2021-04-21 21:50:09 +0100

    Notes almost mapped correctly, not sure why incorrect. Turn red when hovered over

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -70,59 +70,47 @@ public class ScoreDisplay extends PApplet
 		colorMode(RGB);
 		loadScore();
 		printScore();
-		drawNotes();
 	}
 
 	public void draw()
 	{
-		// background(255);
+		background(255);
 		float border = width * 0.1f;
 		for (int i = 0; i < 5; i ++) {
+			stroke(0, 0, 0);
 			float y = map(i, 0, 10, border*2, height - border);
 			line(border, y, width - border, y);
 		}
-		for (int i = 0; i < note.size(); i++) {
-			float x = map(i, 0, (float) note.size(), border, width - border);
 
-			// int j = note.get(i) - '0';
-
-			fill(0);
-			text(note.get(i).getNote(), x, height / 4);
-			// circle(x, height / 2, 20);
-		}
+		drawNotes();
 	}
 
 	void drawNotes()
 	{
-		background(255);
+		// background(255);
 		float border = width * 0.1f;
 
 		for (int i = 0; i < note.size(); i++) {
 			float x = map(i, 0, (float) note.size(), border, width - border);
-			float y = map(i, 0, 8, border*2, height - border);
+			float y = map(note.get(i).getNote() - 60, 0, 8, border*2, border*2+40);
 
-			// int j = note.get(i) - '0';
-
-			fill(0);
 			// circle(x, height / 2, 20);
 			// line(x+10, height / 2, x+10, height/ 2 - 40);
 			// line(x+10, height/ 2 - 40, x+20, height/ 2 - 30);
 
-			if(mouseX == x) {
+			if(pmouseX < x+10 && pmouseX > x-10) {
 				fill(255, 0, 0);
-				circle(x, height / 2, 20);
-				line(x+10, height / 2, x+10, height/ 2 - 40);
-				if(note.get(i).getDuration() == 1) {
-					line(x+10, y - 40, x+20, y - 30);
-				}
+				stroke(255, 0, 0);
 			}
 			else {
-				fill(0);
-				circle(x, y, 20);
-				line(x+10, y, x+10, y - 40);
-				if(note.get(i).getDuration() == 1) {
-					line(x+10, y - 40, x+20, y - 30);
-				}
+				fill(0, 0, 0);
+				stroke(0, 0, 0);
+			}
+			text(note.get(i).getNote(), x, height / 4);
+			circle(x, y, 20);
+			line(x+10, y, x+10, y - 40);
+			if(note.get(i).getDuration() == 1) {
+				line(x+10, y - 40, x+20, y - 30);
 			}
 		}
 	}
